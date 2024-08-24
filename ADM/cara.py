1fbae75188e279987ff69da5be977e6f2ea37b32
MaiorMajor <125076275+MaiorMajor@users.noreply.github.com>
2024-08-24 10:56:04 +0100

    Update cara.py

--- ADM/cara.py
+++ ADM/cara.py
@@ -1,20 +1,21 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
+# Configura as soluções do mp hands e face
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_face_mesh = mp.solutions.face_mesh
 mp_hands = mp.solutions.hands
-
 face_mesh = mp_face_mesh.FaceMesh(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5)
 
+# Configura captura de video com webcam
 cap = cv2.VideoCapture(0)
 cap.set(3, 1024)
 cap.set(4, 768)
 cap.set(cv2.CAP_PROP_FPS, 30)
 
+# Para a gravação num ficheiro mp4
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video_writer = None
 is_recording = False
@@ -22,18 +23,21 @@ fps = 30
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
+# Enquanto captura estiver ativa
 while cap.isOpened():
     success, image = cap.read()
     if not success:
-        print("Ignorando frame vazio.")
+        print("A ignorar frame vazio.")
         continue
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # Converter para RGB
     image_rgb.flags.writeable = False
     results_face = face_mesh.process(image_rgb)
     results_hands = hands.process(image_rgb)
     image_rgb.flags.writeable = True
     black_image = np.zeros((frame_height, frame_width, 3), dtype=np.uint8)
-
+    
+    # Desenhar para cada ponto facial com func draw_landmarks 
     if results_face.multi_face_landmarks:
         for face_landmarks in results_face.multi_face_landmarks:
             mp_drawing.draw_landmarks(
@@ -42,7 +46,8 @@ while cap.isOpened():
                 mp_face_mesh.FACEMESH_TESSELATION,
                 None,
                 mp_drawing.DrawingSpec(color=(255, 255, 255), thickness=1, circle_radius=1))
-
+            
+    # Desenhar pontos de ref mãos com HAND_CONNECTIONS
     if results_hands.multi_hand_landmarks:
         for hand_landmarks in results_hands.multi_hand_landmarks:
             mp_drawing.draw_landmarks(
@@ -56,7 +61,8 @@ while cap.isOpened():
         video_writer.write(black_image)
 
     cv2.imshow('MediaPipe Detection', black_image)
-
+    
+    # Verifica interaçao do utilizador com teclas
     key = cv2.waitKey(1) & 0xFF
     if key == ord('r'):
         if is_recording:
@@ -69,10 +75,10 @@ while cap.isOpened():
             print("Gravação iniciada.")
     elif key == 27:
         break
-
+# Termina gravação
 if is_recording:
     video_writer.release()
-
+# Liberta recursos e fecha programa
 cap.release()
 cv2.destroyAllWindows()
 hands.close()
